d55cca4026e5fb487c0de2d6670388f99bf4d69a
Jeet2561 <144922034+Jeet2561@users.noreply.github.com>
2023-09-30 15:58:52 +0530

    Add files via upload

---
+++ C Language MODULE - 2(1).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int x,y,z,c;
+    printf("Enter the value of X : ");
+    scanf("%d",&x);
+    z=x*x;
+    printf("\nThe square of x is : %d",z);
+    printf("Enter the value of Y : ");
+    scanf("%d",&y);
+    c=y*y*y;
+    printf("\nThe square of y is : %d",c);
+}
